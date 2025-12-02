122866e5a700705ab90a68f562c16ba05c94fe21
Ay-yoobD <dayoobwooday@gmail.com>
2025-12-02 16:27:32 +0200

    first push

---
+++ src/HomePage.jsx
@@ -0,0 +1,15 @@
+function HomePage(){
+
+    return(
+        <div className="FullPage">
+            <div className="MainBody">
+                <div className="WelcomeSection">
+                    <h2>Welcome!</h2>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+export default HomePage;
